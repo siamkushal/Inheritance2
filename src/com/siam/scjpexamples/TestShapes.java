2464baf8d8d3145dd8ddaaa5b513c29746bc0364
siamkushal <siam.kushal@gmail.com>
2019-01-10 02:51:36 +0600

    first commit

---
+++ src/com/siam/scjpexamples/TestShapes.java
@@ -0,0 +1,10 @@
+package com.siam.scjpexamples;
+
+class TestShapes {
+
+    public static void main(String[] args) {
+	    PlayerPiece shape = new PlayerPiece();
+	    shape.displayShape();
+	    shape.movePiece();
+    }
+}
